9d19b4959ba3504ba3f19aafcf72ceb05fb2311f
Bilal <g.bilal034@gmail.com>
2023-09-27 00:55:21 +0500

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,35 @@
+import WorkIm from '../assets/projects/workImg.jpeg'
+import RealEst from '../assets/projects/realestate.jpg'
+
+export const data=[
+    {
+        id:1,
+        name:"Counter App",
+        image:'https://user-images.githubusercontent.com/119156465/264415086-8969ff59-2129-40d3-87ef-b631484e668f.png',
+        github:"https://github.com/itxxBilal/Counter-App",
+        live:"counter-app-eight-dusky.vercel.app/",
+    },
+    {
+        id:2,
+        name:"Ciligraphy Site",
+        image:'https://raw.githubusercontent.com/itxxBilal/icon/main/Home%20menu.png',
+        github:"https://github.com/itxxBilal/Calligraphy-site",
+        live:"calligraphy-site.vercel.app",
+    },
+    {
+        id:3,
+        name:"Portfolio",
+        image:'https://raw.githubusercontent.com/itxxBilal/icon/main/AA.png',
+        github:"https://github.com/itxxBilal/portfolio-website",
+        live:"itxxbilal.github.io/portfolio-website/",
+    },
+    {
+        id:4,
+        name:"React JS Application",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+
+
+]
